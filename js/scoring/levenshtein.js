87bb646a118fda9a8999459944825a8ef2c918e2
David Lewis <david@ideafabrication.za.net>
2013-05-16 09:28:43 -0400

    fixed space bug requirized levenshtein

--- js/scoring/levenshtein.js
+++ js/scoring/levenshtein.js
@@ -13,42 +13,59 @@ http://andrew.hedges.name/resume/
 
 */
 
-// calculate the Levenshtein distance between a and b
-levenshteinenator = function(a,b) {
-    var cost;
-	
-	// get values
-	var m = a.length;
-	
-	var n = b.length;
-	
-	// make sure a.length >= b.length to use O(min(n,m)) space, whatever that is
-	if (m < n) {
-		var c=a;a=b;b=c;
-		var o=m;m=n;n=o;
-	}
-	
-	var r = new Array();
-	r[0] = new Array();
-	for (var c = 0; c < n+1; c++) {
-		r[0][c] = c;
-	}
-	
-	for (var i = 1; i < m+1; i++) {
-		r[i] = new Array();
-		r[i][0] = i;
-		for (var j = 1; j < n+1; j++) {
-			cost = (a.charAt(i-1) == b.charAt(j-1))? 0: 1;
-			r[i][j] = minimator(r[i-1][j]+1,r[i][j-1]+1,r[i-1][j-1]+cost);
-		}
-	}
-	
-	return r[m][n];
-}
+if (typeof(define) === 'undefined') var define = function(name, fn) {
+    this[name] = fn()
+};
 
-// return the smallest of the three values passed in
-minimator = function(x,y,z) {
-	if (x < y && x < z) return x;
-	if (y < x && y < z) return y;
-	return z;
-}
+define('levenshtein', function() {
+    
+    // return the smallest of the three values passed in
+    function minimator(x, y, z) {
+        if (x < y && x < z) return x;
+        if (y < x && y < z) return y;
+        return z;
+    }
+
+    var _export = {
+        // calculate the Levenshtein distance between a and b
+        levenshteinenator: function(a, b) {
+            var cost;
+
+            // get values
+            var m = a.length;
+
+            var n = b.length;
+
+            // make sure a.length >= b.length to use O(min(n,m)) space, whatever that is
+            if (m < n) {
+                var c = a;
+                a = b;
+                b = c;
+                var o = m;
+                m = n;
+                n = o;
+            }
+
+            var r = new Array();
+            r[0] = new Array();
+            for (var c = 0; c < n + 1; c++) {
+                r[0][c] = c;
+            }
+
+            for (var i = 1; i < m + 1; i++) {
+                r[i] = new Array();
+                r[i][0] = i;
+                for (var j = 1; j < n + 1; j++) {
+                    cost = (a.charAt(i - 1) == b.charAt(j - 1)) ? 0 : 1;
+                    r[i][j] = minimator(r[i - 1][j] + 1, r[i][j - 1] + 1, r[i - 1][j - 1] + cost);
+                }
+            }
+
+            return r[m][n];
+        },
+
+
+    };
+    return _export;
+
+});
