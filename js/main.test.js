4df9f53d775925e44c783dc6c8cd55432ee14401
Petrus J. Pretorius <zskilz@gmail.com>
2013-05-17 04:55:52 -0400

    Fixed transforms and some refactoring into new module Desk

--- js/main.test.js
+++ js/main.test.js
@@ -2,11 +2,19 @@ require.config({
     baseUrl: 'js',
     paths: {
         jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
+
     }
+
+
 });
 
 require(['jquery', 'desk', 'morseGen/morseGen', ], function($, desk, morseGen) {
     $(function() {
+        morseGen.makeMorseTable();
         desk.init('mainContent');
+
     });
+
+
+
 });
